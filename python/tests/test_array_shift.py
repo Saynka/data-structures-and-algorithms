e7947934a21e14e28c6246b6288e9902777451d8
Saynka <sankakale@gmail.com>
2020-12-14 17:27:22 -0800

    everything fixed

--- python/tests/test_array_shift.py
+++ python/tests/test_array_shift.py
@@ -1,5 +1,5 @@
-from array_shift import __version__
-from array_shift.array_shift import insertShiftArray
+from code_challenges.array_shift import __version__
+from code_challenges.array_shift.array_shift import insertShiftArray
 
 def test_version():
     assert __version__ == '0.1.0'
@@ -14,6 +14,6 @@ def test_even():
 
 def test_odd():
     actual = insertShiftArray(b,16)
-    expected = [4,8,15,16,23,42]
+    expected = [4,8,16,15,23,42]
     assert actual == expected
 
