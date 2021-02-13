8b0beeca612b5d28f7e2a5d6c0f0675cbf6f9d73
Saynka <sankakale@gmail.com>
2021-02-13 11:37:21 -0800

    push

--- python/code_challenges/array_binary_search/array_binary_search.py
+++ python/code_challenges/array_binary_search/array_binary_search.py
@@ -1,5 +1,8 @@
-# Returns index of x in arryy if present, else -1 
-def BinarySearch(arry, x):
+
+arry = [ 2, 3, 4, 15, 16, 23, 42, 55, 66, 77, 80, 89 ]
+x = 55
+
+def binary_Search(arry, x):
     (left, right) = (0, len(arry) - 1)
     while left <= right:
         mid = (left + right) // 2
@@ -12,14 +15,14 @@ def BinarySearch(arry, x):
             left = mid + 1
     print(-1)
   
-arryy = [ 2, 3, 4, 15, 16, 23, 42, 55, 66, 77, 80 ]
-x = 55
+
+
+result = binary_Search(arry, 0, len(arry) -1, x) 
   
-# # Function call 
-# result = BinarySearch(arryy, 0, len(arryy)-1, x) 
-  
-# if result != -1:
-#     print("Element is present at index", str(result)) 
-# else: 
-#     print("Element is not present")
+if result != -1:
+    print("Element is present at index", str(result)) 
+else: 
+    print("Element is not present")
+
+binary_Search(arry, x)
 
--- python/tests/test_array_binary_search.py
+++ python/tests/test_array_binary_search.py
@@ -1,20 +1,22 @@
-from code_challenges.array_binary_search import __version__
-from code_challenges.array_binary_search.array_binary_search import BinarySearch
+# from code_challenges.array_binary_search import __version__
+from code_challenges.array_binary_search.array_binary_search import binary_Search
 
-def test_version():
-    assert __version__ == '0.1.0'
+# def test_version():
+#     assert __version__ == '0.1.0'
 
-arry = [ 2, 3, 4, 15, 16, 23, 42, 55, 66, 77, 80]
-x = 55
+arry = [ 2, 3, 4, 15, 16, 23, 42, 55, 66, 77, 80, 89 ]
 
 def test_one():
-    actual = BinarySearch(arry, x)
-    expected = 2
+    actual = binary_Search(arry, 7)
+    expected = 55
     assert actual == expected
 
 def test_two():
-    actual = BinarySearch(arry, x)
+    actual = binary_Search(arry, 11)
     expected = -1
     assert actual == expected
 
-
+def test_three():
+    actual = binary_Search(arry, 0)
+    expected = 2
+    assert actual == expected
