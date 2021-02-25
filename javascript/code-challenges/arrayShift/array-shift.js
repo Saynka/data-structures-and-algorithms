d8bccbe1da719ae5bc22ede88d8f0ac5389ca62b
Saynka <sankakale@gmail.com>
2021-02-24 17:09:59 -0800

    console

--- javascript/code-challenges/arrayShift/array-shift.js
+++ javascript/code-challenges/arrayShift/array-shift.js
@@ -6,21 +6,21 @@ let position = 5;
 function shift (arry, position){
   let half = Math.ceil(arry.length / 2) -1;
   let array = [];
-  console.log(arry)
+  // console.log(arry)
   for(let i = 0; i < arry.length; i++){
     // console.log(i, half);
     if (i < half) {
       array.push(arry[i])
   } else if (i === half) {
-      console.log(position)
+      // console.log(position)
     array.push(arry[i])
     array.push(position);
-    console.log(array);
+    // console.log(array);
   } else if ( i > half) {
     array.push(arry[i]);
   }
 }
-console.log(array);
+// console.log(array);
 return array;
 
   // console.log(half);
